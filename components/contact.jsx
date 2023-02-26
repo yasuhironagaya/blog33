5cd63e08511cf24a1063761e8623c11785877449
yasuhironagaya <69679135+yasuhironagaya@users.noreply.github.com>
2023-02-26 15:40:45 +0900

    socialを追加

--- components/contact.jsx
+++ components/contact.jsx
@@ -1,10 +1,12 @@
-import styles from "styles/contact.module.scss"
+import styles from "styles/contact.module.scss";
+import Social from "components/social";
 
 export default function Contact() {
-  return(
+  return (
     <div className={styles.stack}>
       <h3 className={styles.heading}>Contact</h3>
+      <Social iconSize="30px" />
       <address>@nifty.com</address>
     </div>
-  )
+  );
 }
